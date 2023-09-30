8ea40fd7bc53f518b55bd6c11823c74ff769b8c1
FlorianNaef <florian.naef@protonmail.com>
2023-09-30 12:37:50 +0200

    add script and resources

---
+++ VortexBot.py
@@ -0,0 +1,38 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+#Color of center: (255, 219, 195)
+
+while keyboard.is_pressed('q') == False:
+
+    sdx = 0
+
+    posDL = pyautogui.locateOnScreen('dlbutton.png', confidence =.9)
+
+    if posDL != None:
+        time.sleep(1)
+        posDL = pyautogui.locateOnScreen('dlbutton.png', confidence =.9)
+        click(posDL.left + 10, posDL.top + 10)
+
+        time.sleep(2)
+
+        while sdx == 0:
+            posSD = pyautogui.locateOnScreen('sdbutton.png', confidence =.9)
+            if posSD != None:
+                sdx = 1
+                click(posSD.left, posSD.top)
+
+        time.sleep(6)        
+
+    time.sleep(1)
